e5c645fad3584a1cdc06dde4cdb54a8d5ee5a3cd
Igor <igoryan0811@gmail.com>
2020-06-25 14:42:19 +0300

    refactoring application

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,13 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const film1 = prompt("Один из просмотренных фильмов", ""),
-      grade1 = prompt("На сколько оцените его?", ""),
-      film2 = prompt("Один из просмотренных фильмов", ""),
-      grade2 = prompt("На сколько оцените его?", "");
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[film1] = grade1;
-personalMovieDB.movies[film2] = grade2;
+for (let i = 0; i < 2; i++) {
+    const film1 = prompt("Один из просмотренных фильмов", ''),
+          grade1 = prompt("На сколько оцените его?", '');
+    if (film1 != null && grade1 != null && film1.length < 50 && film1 != '' && grade1 != '') {
+        personalMovieDB.movies[film1] = grade1;
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
+
 
 
 console.log(personalMovieDB);
